5be5570dc26b4eaf0e47de5e2a84f1501c433d5b
Excel Koushiroue <koushiroue@gmail.com>
2020-05-26 01:26:23 +0800

    cleaning up...

--- Lolipatter/Form1.Designer.vb
+++ Lolipatter/Form1.Designer.vb
@@ -212,7 +212,7 @@ Partial Class Form1
         Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "LoliPatter v0.1"
+        Me.Text = "LoliPatter v0.1.1"
         Me.TopMost = True
         CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
