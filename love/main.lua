dbaf10262cc75afb7c36beb6e301ac8345fe1451
koushiroue <koushiroue@gmail.com>
2022-06-01 18:55:32 +0800

    aaaaaaaaaaaaaa

--- love/main.lua
+++ love/main.lua
@@ -1,18 +1,31 @@
 local utf8 = require("utf8")
 
+local splashy = require 'libs/splashy'
+
 function love.load()
 	-- lovepotion exculsive config
     -- if we're on Switch, set extended as our current font
     if love._console_name == "Switch" then
         love.graphics.setNewFont("nintendo extended", 14)
     end
-	Image1 = love.graphics.newImage("assets/image/image_0.png")
+	Image1 = love.graphics.newImage("assets/image/image_0.png",Image1)
+	Intro = love.graphics.newImage("assets/image/intro.png",Intro)
 	love.graphics.setNewFont(24)
 	love.graphics.setBackgroundColor(255,255,255)
-end
 
+	splashy.addSplash(Intro, 2)
+	splashy.onComplete(function() print("Hurray! This runs once after the splash screens are over.") end)
+
+end
 function love.draw()
-	love.graphics.draw(Image1, 32, 24)
+
+	splashy.draw()
+	--love.graphics.draw(Image1, 32, 24)
 
 end
 
+function love.update(dt)
+
+	splashy.update(dt)
+
+end
